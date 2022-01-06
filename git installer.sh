0af2dafb81bb2e1db2f97baa2dd958de8caf4c26
gul7491 <76556655+gul7491@users.noreply.github.com>
2022-01-06 13:07:37 +0530

    delete

--- git installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-command -v git >/dev/null 2>&1 ||
-     { echo >&2 "Git is not installed. Installing..";
-             sudo apt update -y
-             sudo apt install git
-
-      }
-GIT_VERSION="$(git --version)"
-echo "git installed version:- " $GIT_VERSION
